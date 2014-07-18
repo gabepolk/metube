73efc4ebbf44ff14200156d4e0c4afee76dd8c96
Gabe Polk <gabe.a.polk@gmail.com>
2014-07-18 09:45:33 -0700

    Added additional movie info to the views

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,11 @@
 class MoviesController < ApplicationController
   def show_matilda
+    @title = 'Matilda'
+    @subtitle = 'Lorem Ipsum'
+    @video_id = 'uGieUGiu978'
   end
   def show_gladiator
+    @title = 'Gladiator'
+    @description = "Russel Crowe is a boss in this video"
   end
 end
